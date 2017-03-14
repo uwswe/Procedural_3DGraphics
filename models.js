90ae58b8761bcebf9d674cdc70d62316621c479a
uwswe <amjenki@uw.edu>
2017-03-13 17:24:37 -0700

    Consolidated 9 for loops into 2. Moved tree model to it's own file

--- models.js
+++ models.js
@@ -2,16 +2,13 @@
  * Created by amjen on 3/13/2017.
  */
 
-//var treeObjects = [];
-function tree(treePos)
-{
+function weepingWillow(treePos) {
     var manager = new THREE.LoadingManager();
     manager.onProgress = function (item, loaded, total) {
         console.log(item, loaded, total);
     };
 
     var texture = new THREE.Texture();
-
 
     var onProgress = function (xhr) {
         if (xhr.lengthComputable) {
@@ -23,28 +20,98 @@ function tree(treePos)
     var onError = function (xhr) {
     };
 
-
     var loader = new THREE.ImageLoader(manager);
-    loader.load('three.js-master/examples/obj/weepingWillow/weeping-willow-t.jpg', function (image) {
+    loader.load('models/vegetation/weepingWillow/weeping-willow-t.jpg', function (image) {
         texture.image = image;
         texture.needsUpdate = true;
     });
 
-// model
-
     var loader = new THREE.OBJLoader(manager);
-    loader.load('three.js-master/examples/obj/weepingWillow/weepingWillow.obj', function (object) {
+    loader.load('models/vegetation/weepingWillow/weepingWillow.obj', function (object) {
         object.traverse(function (child) {
             if (child instanceof THREE.Mesh) {
                 child.material.map = texture;
             }
         });
 
-        //object.position.set(vegetationTilePositions[0].x, vegetationTilePositions[0].y, vegetationTilePositions[0].z);
         object.position.set(treePos.x, .4, treePos.z);
-        console.log("from tree(): " + treePos);
         object.scale.set(.25, .25, .25);
-        //treeObjects.push(object);
+        scene.add(object);
+    }, onProgress, onError);
+}
+
+function oakTree(treePos) {
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function (item, loaded, total) {
+        console.log(item, loaded, total);
+    };
+
+    var texture = new THREE.Texture();
+
+    var onProgress = function (xhr) {
+        if (xhr.lengthComputable) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
+        }
+    };
+
+    var onError = function (xhr) {
+    };
+
+    var loader = new THREE.ImageLoader(manager);
+    loader.load('models/vegetation/OakTree/leaves_02.jpg', function (image) {
+        texture.image = image;
+        texture.needsUpdate = true;
+    });
+
+    var loader = new THREE.OBJLoader(manager);
+    loader.load('models/vegetation/OakTree/OakTree.obj', function (object) {
+        object.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                child.material.map = texture;
+            }
+        });
+
+        object.position.set(treePos.x, .4, treePos.z);
+        object.scale.set(.1, .1, .1);
+        scene.add(object);
+    }, onProgress, onError);
+}
+
+function house(housePos) {
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function (item, loaded, total) {
+        console.log(item, loaded, total);
+    };
+
+    var texture = new THREE.Texture();
+
+    var onProgress = function (xhr) {
+        if (xhr.lengthComputable) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
+        }
+    };
+
+    var onError = function (xhr) {
+    };
+
+    var loader = new THREE.ImageLoader(manager);
+    loader.load('models/structures/house/plaster_bare_6035_8712_Small.jpg', function (image) {
+        texture.image = image;
+        texture.needsUpdate = true;
+    });
+
+    var loader = new THREE.OBJLoader(manager);
+    loader.load('models/structures/house/house1.obj', function (object) {
+        object.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                child.material.map = texture;
+            }
+        });
+
+        object.position.set(housePos.x, .4, housePos.z);
+        object.scale.set(.5, .5, .5);
         scene.add(object);
     }, onProgress, onError);
 }
