7a4aa897a3013083f3183b76313accbaf7cdf956
uwswe <amjenki@uw.edu>
2017-03-13 18:02:59 -0700

    Added necessary libs

--- models.js
+++ models.js
@@ -115,3 +115,41 @@ function house(housePos) {
         scene.add(object);
     }, onProgress, onError);
 }
+
+function stackedRocks(moundPos) {
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function (item, loaded, total) {
+        console.log(item, loaded, total);
+    };
+
+    var texture = new THREE.Texture();
+
+    var onProgress = function (xhr) {
+        if (xhr.lengthComputable) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
+        }
+    };
+
+    var onError = function (xhr) {
+    };
+
+    var loader = new THREE.ImageLoader(manager);
+    loader.load('models/stones/stacked/plaster_bare_6035_8712_Small.jpg', function (image) {
+        texture.image = image;
+        texture.needsUpdate = true;
+    });
+
+    var loader = new THREE.OBJLoader(manager);
+    loader.load('models/stone/stacked/Rock_6.obj', function (object) {
+        object.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                child.material.map = texture;
+            }
+        });
+
+        object.position.set(moundPos.x, .4, moundPos.z);
+        object.scale.set(.5, .5, .5);
+        scene.add(object);
+    }, onProgress, onError);
+}
